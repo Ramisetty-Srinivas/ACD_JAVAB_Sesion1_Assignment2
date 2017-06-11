2e1329484bb60ee2072971f18407633f8f3f6763
Ramisetty-Srinivas <vaasu619@gmail.com>
2017-06-11 22:16:40 +0530

    ACD_JAVAB_Sesion1_Assignment_2

---
+++ src/session1_assignemnt2/SumWithOutUsingPlus.java
@@ -0,0 +1,15 @@
+package session1_assignemnt2;
+
+public class SumWithOutUsingPlus {
+
+	public static void main(String[] args) {
+		
+		int a = 100;
+		int b = 200;
+		int c = a-(-b);
+		
+		System.out.println("The sum of two integers without using plus operator = " +c  );
+		
+	}
+
+}
